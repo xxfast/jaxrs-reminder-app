d61870b9d82047187703bf8a56134a4ed2fec87e
slarre <sebastianl76@gmail.com>
2022-06-28 00:54:45 -0300

    Tests & API functions

--- reminder_srv/reminder/api/routes.py
+++ reminder_srv/reminder/api/routes.py
@@ -1,5 +1,30 @@
+from flask import jsonify
+
 from . import bp
 
-@bp.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@bp.route("/", methods=['POST'])
+def post_item():
+    # TODO verify wrong id
+    
+    return jsonify( { 'id': '1' } )
+
+@bp.route("/<int:id>", methods=['GET'])
+def get_item(id):
+    # TODO verify wrong id
+
+    data = 'TODO: Get data from DB'
+
+    return jsonify( { 'data': data } )
+
+@bp.route("/<int:id>", methods=['PUT'])
+def put_item(id):
+    # TODO verify wrong id
+
+    return jsonify( {} )
+
+@bp.route("/<int:id>", methods=['DELETE'])
+def delete_item(id):
+    # TODO verify wrong id
+
+    return jsonify( {} )
+
--- reminder_srv/reminder/tests/test_service.py
+++ reminder_srv/reminder/tests/test_service.py
@@ -1,3 +1,87 @@
-def test_hello(client):
-    response = client.get('/')
-    assert response.data == b'<p>Hello, World!</p>'
+def get_item(client, id):
+    return client.get('/' + id)
+
+def post_item(client, data):
+    response = client.post('/', json={"data":data})
+
+    return response
+
+def put_item(client, id, data):
+    response = client.put('/' + id, json={"data":data})
+
+    return response
+
+def delete_item(client, id):
+    return client.delete('/' + id)
+
+def test_wrong_item(client):
+    # GET wrong item
+    response = get_item(client, '1')
+    assert response.status_code == 404
+
+    # PUT wrong item
+    response = put_item(client, '1', '')
+    assert response.status_code == 404
+
+    # DELETE wrong item
+    response = delete_item(client, '1')
+    assert response.status_code == 404
+
+def test_post_new_item(client):
+    item_data = "New item - test_post_new_item"
+
+    # GET item num 1
+    response = get_item(client, '1')
+    assert response.status_code == 404
+
+    # POST a new item
+    response = post_item(client, item_data)
+    assert response.status_code == 200
+
+    # It must be item num 1
+    assert response.json["id"] == '1'
+
+    # GET item num 1
+    response = get_item(client, '1')
+    assert response.status_code == 200
+
+    # It must respond same item info 
+    assert response.json["data"] == item_data
+
+def test_edit_item(client):
+    item_data = "New item - test_edit_item"
+
+    # Ensure it has an item
+    response = post_item(client, item_data)
+    assert response.status_code == 200
+    item_id = response.json["id"]
+
+    # EDIT data
+    edit_data = "Edited item - test_edit_item"
+    response = put_item(client, item_id, item_data)
+    assert response.status_code == 200
+
+    # It must respond same item new info 
+    response = get_item(client, '1')
+    assert response.status_code == 200
+    assert response.json["data"] == edit_data
+
+def test_delete_item(client):
+    item_data = "New item - test_delete_item"
+
+    # Ensure it has an item
+    response = post_item(client, item_data)
+    assert response.status_code == 200
+    item_id = response.json["id"]
+
+    # Confirm by getting it
+    response = get_item(client, item_id)
+    assert response.status_code == 200
+
+    # DELETE item
+    response = delete_item(client, item_id)
+    assert response.status_code == 200
+
+    # It shouldn't found item
+    response = get_item(client, item_id)
+    assert response.status_code == 404
